6aeffc70a7349e1460fa9775b2272ff242032267
Igor Caetano <72323075+IgorCaetano@users.noreply.github.com>
2025-05-06 17:23:21 -0300

    Atualização nos testes para motores
    Adição de mais um motor no código de testes. Agora, ao executar o código, serão girados os 2 motores ao mesmo tempo.

--- Tests/nema17_tb6600/nema17_tb6600.ino
+++ Tests/nema17_tb6600/nema17_tb6600.ino
@@ -1,28 +1,42 @@
 // Vídeo para se ter como base na hora da compreensão da execução do processo todo
 // https://www.youtube.com/watch?v=nLV0fjUWI-g&ab_channel=HenryCawley
 
-#define directionPin 2
-#define stepPin 3
+
+#define directionPin1 2
+#define stepPin1 3
+#define directionPin2 4
+#define stepPin2 5
+
 
 #define stepsPerRevolution 6400
 
+
 void setup() {
-    pinMode(directionPin,OUTPUT);
-    pinMode(stepPin,OUTPUT);
+    pinMode(directionPin1,OUTPUT);
+    pinMode(stepPin1,OUTPUT);
+    pinMode(directionPin2,OUTPUT);
+    pinMode(stepPin2,OUTPUT);
 }
+
 
 void loop() {
     for (int i = 0; i < 5 * stepsPerRevolution; i++){ // Número 5 indica quantidade de rotações
-        digitalWrite(directionPin, HIGH); // HIGH pra uma direção / LOW para a outra direção
-        
-        digitalWrite(stepPin, HIGH);
+        digitalWrite(directionPin1, HIGH); // HIGH pra uma direção / LOW para a outra direção
+        digitalWrite(directionPin2, HIGH); // HIGH pra uma direção / LOW para a outra direção
+       
+        digitalWrite(stepPin1, HIGH);
+        digitalWrite(stepPin2, HIGH);
         delayMicroseconds(50); // Diminuir o delay aumenta a frequência do sinal do degrau, que vai fazer o degrau do motor mais rápido
 
-        digitalWrite(stepPin,LOW);
+
+        digitalWrite(stepPin1,LOW);
+        digitalWrite(stepPin2,LOW);
         delayMicroseconds(50); // Diminuir o delay aumenta a frequência do sinal do degrau, que vai fazer o degrau do motor mais rápido
     }
+
 
     // Adicionado para parar as rotações do motor (o loop fica preso dentro do while)
     while(1){
     }
 }
+
